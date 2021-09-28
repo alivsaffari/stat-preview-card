44d07b08643875c408d9764ba02338f7e005e59d
Ali Saffari <parvazled@gmail.com>
2021-09-28 12:14:13 +0330

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    theme: {
+        fontFamily: {
+            'sans': ['Inter', 'sans-serif'],
+            'stat': ['Lexend\\ Deca', 'sans-serif']
+        },
+        colors: {
+            'mainBackground': '#090b1a',
+            'cardBackground': '#1b1938',
+            'accent': '#aa5cdb',
+            'mainHeading-stats': 'hsl(0, 0%, 100%)',
+            'mainParagraph': 'hsla(0, 0%, 100%, 0.75)',
+            'statHeadings': 'hsla(0, 0%, 100%, 0.6)'
+        }
+    }
+}
